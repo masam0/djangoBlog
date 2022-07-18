05e801831ca5d045537535bc2a4880259dd1b00e
masam0 <masaya.esp@gmail.com>
2022-07-18 14:05:48 +0900

    Create template html

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return HttpResponse("This will be the index of my blog")
+  return render(request, 'blog/index.html', {})
