5bf04696b4b2451d504f124e1eee2be0b3e08257
masam0 <masaya.esp@gmail.com>
2022-07-18 21:07:47 +0900

    Add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
+  date_published = models.DateTimeField('date published')
+  date_updated = models.DateTimeField('date updated', null=True)
+  content = models.TextField()
+  is_published = models.BooleanField()
+
+  def __str__(self):
+      return self.title
+
+class Tag(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+  
+  def __str__(self):
+    return self.title
+
+class Comment(models.Model):
+  comment = models.TextField()
+  date_published = models.DateTimeField()
+
+  def __str__(self):
+    return self.title
+
+class Tagged(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.PROTECT)
+  tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+
+class Commented(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
