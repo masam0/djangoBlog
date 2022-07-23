b3c940f2eb48b18b3df68a0ecae6f09f98b07ab4
masam0 <masaya.esp@gmail.com>
2022-07-23 17:16:46 +0900

    Add article view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Tag
 
 # Create your views here.
@@ -15,3 +15,8 @@ def index(request):
   latest_posts = Post.objects.order_by('-date_published')[:5]
   context["latest_posts"] = latest_posts
   return render(request, 'blog/index.html', context)
+
+def article(request, slug):
+  article = get_object_or_404(Post, slug=slug)
+  context["article"] = article
+  return render(request, 'blog/article.html', context)
