ba69ffb4e6c214ac33e92af91b7ccd193e9b32b0
masam0 <masaya.esp@gmail.com>
2022-07-23 16:44:38 +0900

    Add default context for navigation

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Tag
 
 # Create your views here.
 
+tags = Tag.objects.all()
+years = Post.objects.dates('date_published', 'year')
+context = {
+  "tags": tags,
+  "years": years,
+}
+
 def index(request):
   latest_posts = Post.objects.order_by('-date_published')[:5]
-  context = {
-    "latest_posts": latest_posts,
-  }
+  context["latest_posts"] = latest_posts
   return render(request, 'blog/index.html', context)
